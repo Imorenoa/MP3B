9aacdb5f82ccf3f140d3957712b95ad6a41e93c4
Imorenoa <moreno.igor.87@gmail.com>
2021-04-14 19:49:54 +0200

    Esqueleto

--- MP3B/src/GastoException.java
+++ MP3B/src/GastoException.java
@@ -1,4 +1,4 @@
-package pacdesarrollo;
+
 
 public class GastoException {
 	public GastoException() {
--- MP3B/src/pacdesarrollo/Main.java
+++ MP3B/src/pacdesarrollo/Main.java
@@ -2,8 +2,11 @@ package pacdesarrollo;
 
 public class Main {
 
+	//MINUTO 51:30 DEL VIDEOTUTORIAL
+	
 	public static void main(String[] args) {
 		Gasto gasto = new Gasto(2345, "Alquiler");
+		
 		/*gasto.setDescriptiopn("Alquiler");
 		gasto.setDinero(500.0);*/
 		
